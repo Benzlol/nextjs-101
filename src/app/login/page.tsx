088c0c9c5363c17c68cb9fd60f0a2d5c9426fd10
unknown <stu494306@gmail.com>
2024-03-07 18:01:22 +0700

    upate

---
+++ src/app/login/page.tsx
@@ -0,0 +1,113 @@
+"use client"
+
+// import { useEffect, useState } from "react";
+import Image from "next/image";
+import TextField from '@mui/material/TextField';
+import React from "react";
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { useFormState } from "react-dom";
+import { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function Loginpage() {
+    const [showPassword, setShowPassword] = React.useState(false);
+const [email, setemail] = useState("");
+const [password, setPassword] = useState("");   
+
+const router = useRouter();
+const Login = async () => {
+    try{
+    const responseLogin = await axios.post("https://4d65-2403-6200-8821-1051-413-3023-7a17-abba.ngrok-free.app/login/",
+    {
+        username: email,
+        password: password
+    })
+    console.log(responseLogin)
+
+    if(responseLogin.status === 200){
+        router.push("/")
+    }
+} 
+    catch (error) {
+        console.log('Error from func Register: ', error)
+    }
+}
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+    // const [random, setRandom] = useState("");
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         GetRandomColor();
+    //     }, 1000);
+
+    //     return () => clearInterval(interval);
+    // }, []);
+
+    // const GetRandomColor = () => {
+    //     const randomColor = "#" + ((1 << 24) * Math.random() | 0).toString(16);
+    //     setRandom(randomColor);
+    // }
+
+    return (
+        <div className="flex justify-center items-center w-full h-screen bg-gray-200">
+            <div className="flex flex-col justify-evenly rounded-xl w-[450px] h-[620px] bg-white shadow-md">
+
+                <div className="flex justify-center items-center h-[100%]">
+                    <div className="flex justify-center items-center flex-col gap-8">
+                        <h1 className="font-extrabold text-3xl">Welcome Benz</h1>
+                        <Image className="hover:w-24 hover:h-24" alt="" src="next.svg" width={70} height={70} />
+                    </div>
+                </div>
+
+                <div className="h-[100%] flex flex-col gap-4 items-center">
+                    <TextField className="w-[270px]" id="standard-basic" label="Email" variant="standard" value={email} onChange={(e) => setemail(e.target.value)}/>
+                    <FormControl className="w-[270px]" sx={{ m: 1, width: '25ch' }} variant="standard">
+                        <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                        <Input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            id="standard-adornment-password"
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
+                </div>
+
+                <div className="h-[100%]  flex flex-col items-center justify-between">
+                    <button onClick={Login} className="font-bold text-white  w-[270px] h-[40px] rounded-3xl bg-gradient-to-r from-sky-300 via-violet-600 to-fuchsia-400 hover:scale-110">LOGIN</button>
+                    <div className="flex">
+                    <p className="mb-[20px] text-gray-500 mr-[5px]">Don't have an accout?</p>
+                    <Link href={'register'} className="mb-[20px] hover:text-blue-700">Sign up</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+// md:w-[350px] md:h-[500px] lg:w-[500px] lg:h-[570px]
---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import Link from 'next/link';
+import { useState } from 'react';
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(n => n + 1);
+  };
+
+  const decrease = () => {
+    setCount(d => d - 1);
+  };
+
+  const Name = "Best";
+
+  return (
+    <div className='border-4 border-red-700 flex w-full justify-center items-center h-screen'>
+      <div className='border-4 border-green-700 h-fit'>
+        <h1 className='text-yellow-700'>Home</h1>
+        <p>{count}</p>
+        <button onClick={increase}>Increase</button>
+        <br />
+        <button onClick={decrease}>Decrease</button>
+      </div>  
+      {/* <Link href={`/dynamic/${Name}`}>Go to Dynamic page</Link> */}
+    </div>
+  );
+}
